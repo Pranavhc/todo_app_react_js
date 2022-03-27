cb554de624d953d7932699d3adf1f1d18a9e6a47
Pranav Chandane <pranavhchandane@gmail.com>
2022-03-27 16:42:07 +0530

    small tweaks in css and animations

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,9 +7,7 @@ const Content = ({ tasks, handleCheck, handleDelete }) => {
         <main>
             {tasks.length ?
                 (tasks.map(task => <Task key={task.id} task={task} handleDelete={handleDelete} handleCheck={handleCheck} />)) :
-                <Fade top>
-                    <h2 style={{ textAlign: "center", marginTop: "4rem" }}>No Tasks To Show</h2>
-                </Fade>
+                <Fade top><h2 style={{ textAlign: "center", marginTop: "4rem" }}>No Tasks To Show</h2></Fade>
             }
         </main>
     )
